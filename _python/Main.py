7824ec48be7a25abdcbcb0c6108393383332c9d6
Jerry <jerry@cosecloud.com>
2020-03-10 15:09:53 -0500

    update

--- _python/Main.py
+++ _python/Main.py
@@ -9,6 +9,7 @@ import Settings
 import Commands
 import Threads
 import Functions
+import Call_Thread
 
 # import UI functions
 import UI_Update
@@ -29,175 +30,6 @@ date = time.strftime('%m_%d_%Y')
 
 
 class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
-
-    def start_motion_preset(self):
-        if not Settings.motionPreset_running:
-            try:
-                Settings.gravitropism_wait = self.gravitropism_spinBox.value()
-                Settings.rotateAmount = self.rotateAmount_spinBox.value()
-                Settings.rotateDelay = self.rotateDelay_spinBox.value()
-                Settings.motionPreset_mode = self.motionPreset_tabWidget.currentIndex()
-
-                self.MPreset_Thread = Threads.MPreset()
-                self.MPreset_Thread.started.connect(
-                    lambda: UI_Update.motionPreset_update(self))
-                self.MPreset_Thread.finished.connect(
-                    lambda: UI_Update.motionPreset_update(self))
-                self.MPreset_Thread.start()
-
-            except Exception as e:
-                print(e)
-        else:
-            Settings.motionPreset_running = False
-            UI_Update.motionPreset_update(self)
-
-    def start_cycle(self):
-        if not Settings.cycle_running:
-            try:
-                Settings.cycle_time = self.powerCycle_spinBox.value()
-
-                self.Cycle_Thread = Threads.Cycle()
-                self.Cycle_Thread.started.connect(
-                    lambda: UI_Update.cycle_update(self))
-                self.Cycle_Thread.start()
-
-            except Exception as e:
-                print(e)
-        else:
-            Settings.cycle_running = False
-            UI_Update.cycle_update(self)
-
-    def schedule_test(self):
-        if not Settings.test_running:
-            try:
-                Settings.angle_1 = self.rotate1_spinbox.value()
-                Settings.angle_2 = self.rotate2_spinbox.value()
-                self.Test_Thread = Threads.Test()
-                self.Test_Thread.started.connect(
-                    lambda: UI_Update.test_update(self))
-                self.Test_Thread.finished.connect(
-                    lambda: UI_Update.test_update(self))
-                self.Test_Thread.start()
-            except Exception as e:
-                print(e)
-        else:
-            Settings.test_running = False
-            UI_Update.test_update(self)
-
-    def schedule_run(self):
-        if not Settings.sch_running:
-            try:
-                Settings.angle_1 = self.rotate1_spinbox.value()
-                Settings.angle_2 = self.rotate2_spinbox.value()
-                Settings.delay_1 = self.wait1_spinbox.value()
-                Settings.delay_2 = self.wait2_spinbox.value()
-
-                self.Schedule_Thread = Threads.Schedule()
-                self.Schedule_Thread.started.connect(
-                    lambda: UI_Update.schedule_update(self))
-                self.Schedule_Thread.start()
-            except Exception as e:
-                print(e)
-        else:
-            Settings.sch_running = False
-            UI_Update.schedule_update(self)
-
-    def start_snapshot(self):
-        try:
-            Functions.Camera_update(self)
-            self.Snap_Thread = Threads.Snap()
-            self.Snap_Thread.started.connect(
-                lambda: UI_Update.imaging_disable(self))
-            self.Snap_Thread.finished.connect(
-                lambda: UI_Update.update_frame_snap(self, "../_temp/snapshot.jpg"))
-            self.Snap_Thread.start()
-
-        except Exception as e:
-            print(e)
-
-    def start_livefeed(self):
-        try:
-            Settings.livetime = self.liveFeed_spinBox.value()
-            self.livefeed_Thread = Threads.Live()
-            self.livefeed_Thread.started.connect(
-                lambda: UI_Update.imaging_disable(self))
-            self.livefeed_Thread.finished.connect(
-                lambda: UI_Update.imaging_enable(self))
-            self.livefeed_Thread.start()
-
-        except Exception as e:
-            print(e)
-
-    def start_preview(self):
-        try:
-            Functions.Camera_update(self)
-            self.Preview_Thread = Threads.Preview()
-            self.Preview_Thread.started.connect(
-                lambda: UI_Update.imaging_disable(self))
-            if(Settings.image_format):
-                self.Preview_Thread.finished.connect(
-                    lambda: UI_Update.update_frame_alt(self, "../_temp/preview.jpg"))
-            else:
-                self.Preview_Thread.finished.connect(
-                    lambda: UI_Update.update_frame_alt(self, "../_temp/preview.png"))
-            self.Preview_Thread.start()
-
-        except Exception as e:
-            print(e)
-
-    def rotate_image(self):
-        try:
-            Functions.Camera_update(self)
-            Settings.rotation += 1
-            self.Snap_Thread = Threads.Snap()
-            self.Snap_Thread.started.connect(
-                lambda: UI_Update.imaging_disable(self))
-            self.Snap_Thread.finished.connect(
-                lambda: UI_Update.update_frame(self, "../_temp/snapshot.jpg"))
-            self.Snap_Thread.start()
-
-        except Exception as e:
-            print(e)
-
-    def start_sequence(self):
-
-        if(Settings.image_format):
-            Settings.file = Settings.full_dir + "/" + Settings.sequence_name + "_%04d.jpg"
-        else:
-            Settings.file = Settings.full_dir + "/" + Settings.sequence_name + "_%04d.png"
-        self.Progress_Bar.setMaximum(Settings.total)
-
-        try:
-            if not Settings.timelapse_running:
-                Functions.Camera_update(self)
-
-                self.Imaging_Thread = Threads.Image()
-                self.Imaging_Thread.started.connect(
-                    lambda: UI_Update.timelapse_disable(self))
-                self.Imaging_Thread.finished.connect(
-                    lambda: UI_Update.timelapse_enable(self))
-                self.Imaging_Thread.capturing.connect(
-                    lambda: UI_Update.imaging_disable(self))
-                self.Imaging_Thread.complete.connect(
-                    lambda: UI_Update.update_frame(self, Settings.current_image))
-                self.Imaging_Thread.start()
-            else:
-                Settings.timelapse_running = False
-                UI_Update.timelapse_enable(self)
-
-        except Exception as e:
-            print(e)
-
-        if(Settings.storage_mode):
-            try:
-                self.Dropbox_Thread = Threads.Dropbox()
-                self.Dropbox_Thread.start()
-
-                self.Email_Thread = Threads.Email()
-                self.Email_Thread.start()
-
-            except Exception as e:
-                print(e)
  # access variables inside of the UI's file
 
     def __init__(self):
@@ -229,12 +61,12 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
         self.pulse_pushButton.clicked.connect(lambda: Commands.pulse_run(self))
 
         self.confirmCycle_pushButton.clicked.connect(
-            lambda: self.start_cycle())
+            lambda: Call_Thread.start_cycle(self))
 
         self.schedulerTest_pushButton.clicked.connect(
-            lambda: self.schedule_test())
+            lambda: Call_Thread.schedule_test(self))
         self.schedulerSet_pushButton.clicked.connect(
-            lambda: self.schedule_run())
+            lambda: Call_Thread.schedule_run(self))
         self.motorSpeed_slider.valueChanged.connect(
             lambda: Commands.motorSliderChange(self))
         self.motorSpeed_slider.sliderReleased.connect(
@@ -242,24 +74,25 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
 
         self.clinostatSet_pushButton.clicked.connect(
             lambda: Commands.clinoStart(self))
-        self.snapshot_pushButton.clicked.connect(lambda: self.start_snapshot())
-        self.liveFeed_pushButton.clicked.connect(lambda: self.start_livefeed())
-        self.preview_pushButton.clicked.connect(lambda: self.start_preview())
-        self.x_resolution_spinBox.valueChanged.connect(
-            lambda: self.update_resolution())
-        self.y_resolution_spinBox.valueChanged.connect(
-            lambda: self.update_resolution())
-        self.rotate_pushButton.clicked.connect(lambda: self.rotate_image())
+        self.snapshot_pushButton.clicked.connect(
+            lambda: Call_Thread.start_snapshot(self))
+        self.liveFeed_pushButton.clicked.connect(
+            lambda: Call_Thread.start_livefeed(self))
+        self.preview_pushButton.clicked.connect(
+            lambda: Call_Thread.start_preview(self))
+
+        self.rotate_pushButton.clicked.connect(
+            lambda: Call_Thread.rotate_image(self))
 
         self.xAxis_horizontalSlider.valueChanged.connect(
             lambda: Functions.zoomSliderChange(self))
         self.xAxis_horizontalSlider.sliderReleased.connect(
-            lambda: self.start_snapshot())
+            lambda: Call_Thread.start_snapshot(self))
 
         self.yAxis_horizontalSlider.valueChanged.connect(
             lambda: Functions.zoomSliderChange(self))
         self.yAxis_horizontalSlider.sliderReleased.connect(
-            lambda: self.start_snapshot())
+            lambda: Call_Thread.start_snapshot(self))
 
         self.motorConfirm_pushButton.clicked.connect(
             lambda: Commands.motor_rotate(self.motor_spinBox.value()))
@@ -285,7 +118,7 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
         self.storage_tabWidget.currentChanged.connect(
             lambda: UI_Update.validate_input(self))
         self.startRoutines_pushButton.clicked.connect(
-            lambda: self.start_sequence())
+            lambda: Call_Thread.start_sequence(self))
 
         self.JPG_radioButton.toggled.connect(
             lambda: Functions.img_format(self))
@@ -295,7 +128,7 @@ class MainWindow(QMainWindow, FlashLapse_UI.Ui_MainWindow):
         self.lightingPreset_pushButton.clicked.connect(
             lambda: Functions.start_lighting_preset(self))
         self.MotionPreset_pushButton.clicked.connect(
-            lambda: self.start_motion_preset())
+            lambda: Call_Thread.start_motion_preset(self))
 
 
 # main function
