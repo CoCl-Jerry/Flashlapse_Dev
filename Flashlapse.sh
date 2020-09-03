6fa3c4bbaddbe940ab30ba0f93316a7359d276d5
Jerry <jerry@cosecloud.com>
2020-09-03 12:21:15 -0500

    Update Flashlapse.sh

--- Flashlapse.sh
+++ Flashlapse.sh
@@ -1,8 +1,8 @@
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-	cd /home/pi/Flashlapse_Dev
+	cd /home/pi/Documents/Flashlapse_Dev
 	git pull
 fi
 
-cd /home/pi/Flashlapse_Dev/_python
+cd /home/pi/Documents/Flashlapse_Dev/_python
 sudo python3 Main.py
