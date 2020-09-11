187c805613b565daacae47b383418190b75c8799
Jerry <jerry@cosecloud.com>
2020-09-10 23:01:44 -0500

    Update Threads.py

--- _python/Threads.py
+++ _python/Threads.py
@@ -253,18 +253,19 @@ class Sensor(QThread):
                 Settings.DHT_SENSOR, 18)
             if Settings.humidity is not None and Settings.temperature is not None:
                 self.update.emit()
-
-                if(not os.path.isdir(Settings.prelog_dir)):
-                    os.umask(0)
-                    os.mkdir(Settings.prelog_dir)
-                if(not os.path.isdir(Settings.log_dir)):
-                    os.umask(0)
-                    os.mkdir(Settings.log_dir)
-                log_file = open(Settings.log_dir + "/log.txt", "w")
-                os.chmod(Settings.log_dir + "/log.txt", 0o777)
-                now = datetime.now()
-                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-                log_file.write(dt_string + "\t" + "{0: 0.1f}".format(Settings.temperature) + "\t" +
-                               "{0: 0.1f}".format(Settings.humidity) + "\n")
+                if(Settings.log_sensor):
+                    if(not os.path.isdir(Settings.prelog_dir)):
+                        os.umask(0)
+                        os.mkdir(Settings.prelog_dir)
+                    if(not os.path.isdir(Settings.log_dir)):
+                        os.umask(0)
+                        os.mkdir(Settings.log_dir)
+                    log_file = open(Settings.log_dir + "/log.txt", "w")
+                    os.chmod(Settings.log_dir + "/log.txt", 0o777)
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    log_file.write(dt_string + "\t" + "{0: 0.1f}".format(Settings.temperature) + "\t" +
+                                   "{0: 0.1f}".format(Settings.humidity) + "\r\n")
+                    log_file.close()
 
             sleep(Settings.sample_time)
