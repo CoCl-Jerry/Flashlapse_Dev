a6c139938eb38b54f4bf901859e7e39c21d9fd52
Jerry <jerry@cosecloud.com>
2020-09-10 20:49:58 -0500

    Update Commands.py

--- _python/Commands.py
+++ _python/Commands.py
@@ -40,7 +40,7 @@ def startup():
 def deploy_lights(temp_list):
     for x in temp_list:
         send_CMD(x)
-        sleep(0.1)
+        sleep(1)
     current_CMD = "4\n"
     send_CMD(current_CMD)
 
